316acfc64765ba04d6841771d6a002f030437616
michele <michele@gumgum.com>
2017-10-19 14:11:53 -0700

    add missing files

--- src/videoScreen.js
+++ src/videoScreen.js
@@ -4,6 +4,18 @@ AFRAME.registerComponent('video-screen', {
         name: { type: 'string', default: '' }
     },
     update: function () {
-
+        const { name } = this.data;
+        this.el.setAttribute('id', `curved_screen_obj_${name}`);
+        // this.el.setAttribute('obj-model', 'obj: url(models/screen/screen_00.obj)');
+        // this.el.setAttribute('position', { x: 0, y: 0, z: 0 });
+        // this.el.setAttribute('rotation', { x: 0, y: 0, z: 0 });
+        // this.el.setAttribute('scale', { x: 0.044, y: 0.044, z: 0.044 });
+        // this.el.setAttribute('shadow', 'cast: false; receive: false;');
+        // this.el.setAttribute('material', 'src', '#video-clothes');
+        // this.el.setAttribute('material', {
+        //     shader: 'flat',
+        //     src: '#video',
+        //     side: 'front'
+        // });
     }
 });
